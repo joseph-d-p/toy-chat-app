c53ec56a357f444bb0c6abe3bc6cff7c154cacba
joseph-d-p <josephandrewdelapena@gmail.com>
2022-10-26 18:16:11 +0800

    fix(client): Add simple socket connection in client

--- client/src/chat-app.js
+++ client/src/chat-app.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 
 function ChatApp(props) {
-  return <span>Hello World!</span>;
+  const [isConnected, setConnected] = useState(false);
+  const [sockets, addSocket] = useState([]);
+
+  const connectionHandler = _ => {
+    const url = "http://localhost:4000";
+    const socket = io(url, { autoConnect: false });
+
+    socket.connect();
+    socket.on("connect", () => {
+      addSocket(sockets => [...sockets, socket.id]);
+      setConnected(socket.connected);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("disconnected: ", socket.id);
+    });
+  }
+
+  return (
+    <>
+      <h2>Connected Socket IDs:</h2>
+      {sockets.map(
+        (id, index) => <div key={index}>{id}</div>
+      )}
+      <span>
+        <button onClick={connectionHandler}>
+          Connect
+        </button>
+      </span>
+    </>
+  );
 }
 
 export default ChatApp;
