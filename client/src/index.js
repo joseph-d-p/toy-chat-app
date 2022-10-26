5326bef2f9e5f80657c0b10bd2711f7c82c2fcf5
joseph-d-p <josephandrewdelapena@gmail.com>
2022-10-26 18:15:46 +0800

    refactor(client): Use ReactDOM client

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { createRoot } from 'react-dom/client';
+
 import ChatApp from "./chat-app";
 
-const root = ReactDOM.createRoot(
-  document.querySelector("#root")
-);
-root.render(<ChatApp />);
+function App(props) {
+  return (
+    <div>
+      <ChatApp />
+    </div>
+  );
+}
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(<App />);
