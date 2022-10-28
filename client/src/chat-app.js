d5b93e332fe91dd854db708996f70c444b3d9df0
joseph-d-p <josephandrewdelapena@gmail.com>
2022-10-28 13:37:00 +0800

    refactor(client): Add capability to join room

--- client/src/chat-app.js
+++ client/src/chat-app.js
@@ -3,18 +3,22 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 function ChatApp(props) {
-  const [isConnected, setConnected] = useState(false);
-  const [sockets, addSocket] = useState([]);
+  const [connections, setConnected] = useState({});
+  const [room, setRoom] = useState("");
 
   const connectionHandler = _ => {
-    const url = "http://localhost:4000";
+    const url = `http://localhost:4000`;
     const socket = io(url, { autoConnect: false });
 
     socket.connect();
-    socket.on("connect", () => {
-      addSocket(sockets => [...sockets, socket.id]);
-      setConnected(socket.connected);
+    socket.on("connect", (socket) => {
+      setConnected(cx => ({
+        ...cx,
+        [room]: true
+      }));
     });
+
+    socket.emit("join", room);
 
     socket.on("disconnect", () => {
       console.log("disconnected: ", socket.id);
@@ -23,14 +27,17 @@ function ChatApp(props) {
 
   return (
     <>
-      <h2>Connected Socket IDs:</h2>
-      {sockets.map(
-        (id, index) => <div key={index}>{id}</div>
-      )}
+      <h2>Toy Chat App</h2>
+      <div>
+        <input
+          name="room"
+          type="text"
+          onChange={e => setRoom(e.target.value)}
+        />
+        <input type="button" onClick={connectionHandler} value="Join" />
+      </div>
       <span>
-        <button onClick={connectionHandler}>
-          Connect
-        </button>
+        {connections[room] ? `Joined Room: ${room}` : "Offline"}
       </span>
     </>
   );
